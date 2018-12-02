502b7e9ad9b8ffe41b9516de0c287a4bfbb34bd0
Charles Buczel <charles.buczel@gmail.com>
2018-12-02 22:48:07 +0000

    Day 01A

--- 2018/day_01a.exs
+++ 2018/day_01a.exs
@@ -1,18 +1,12 @@
-
+ 
 defmodule Day1A do
-    def chronal_calibration(input) do
-        input
-        |> String.split("\n", trim: true)
-        |> sum_lines(0)
-    end
-
-    defp sum_lines([line | lines], current_frequency) do
-        new_frequency = String.to_integer(line) + current_frequency
-        sum_lines(lines, new_frequency)
-    end
-
-    defp sum_lines([], current_frequency) do
-        current_frequency
+    def chronal_calibration(stream) do
+        stream
+        |> Stream.map(fn line ->
+            {integer, _leftover} = Integer.parse(line)
+            integer
+        end)
+        |> Enum.sum()  
     end
 end
 
@@ -26,45 +20,56 @@ case System.argv() do
             import Day1A
 
             test "chronal_calibration positives" do
-                assert chronal_calibration("""
+                {:ok, io} = StringIO.open("""
                 +1
                 +1
                 +1
-                """) == 3
+                """)
+
+                assert chronal_calibration(IO.stream(io, :line)) == 3
             end
 
             test "chronal_calibration negatives" do
-                assert chronal_calibration("""
+                {:ok, io} = StringIO.open("""
                 -1
                 -2
                 -3
-                """) == -6
+                """)
+
+                assert chronal_calibration(IO.stream(io, :line)) == -6
             end
 
             test "chronal_calibration no args" do
-                assert chronal_calibration("""
-                """) == 0
+                {:ok, io} = StringIO.open("""
+                """)
+
+                assert chronal_calibration(IO.stream(io, :line)) == 0
             end
 
             test "chronal_calibration mixed args" do
-                assert chronal_calibration("""
-                1
+                {:ok, io} = StringIO.open("""
+                +1
                 3
                 -8
-                """) == -4
+                """)
+
+                assert chronal_calibration(IO.stream(io, :line)) == -4
             end
 
             test "chronal_calibration zeroes" do
-                assert chronal_calibration("""
+                {:ok, io} = StringIO.open("""
                 0
-                0
-                """) == 0
+                +0
+                -0
+                """)
+
+                assert chronal_calibration(IO.stream(io, :line)) == 0
             end
         end
 
     [input_file] -> 
         input_file
-        |> File.read!()
+        |> File.stream!([], :line)
         |> Day1A.chronal_calibration()
         |> IO.puts
 
