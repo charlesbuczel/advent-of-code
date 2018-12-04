14fd102c19696c898dcf8b364e79363839629ecb
Charles Buczel <charles.buczel@gmail.com>
2018-12-04 23:58:34 +0000

    Day 02 - mix project

---
+++ 2018/day02/lib/day02.ex
@@ -0,0 +1,18 @@
+defmodule DAY02 do
+  @moduledoc """
+  Documentation for DAY02.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DAY02.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2018/day02/test/day02_test.exs
@@ -0,0 +1,8 @@
+defmodule DAY02Test do
+  use ExUnit.Case
+  doctest DAY02
+
+  test "greets the world" do
+    assert DAY02.hello() == :world
+  end
+end
